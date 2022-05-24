c2c8a8d382de8cc020663dcabc2d72783bb5ea57
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-05-24 12:17:07 -0300

    ejercicio1

---
+++ myapp/myapp/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("ejercicio 1");
+int numeroAInvertir = 0;
+int numeroInvertido = 0;
+
+do
+{
+    Console.WriteLine("Ingrese el numero a invertir\n");
+    numeroAInvertir = Convert.ToInt32(Console.ReadLine());
+} while(numeroAInvertir < 0);
+
+
+while (numeroAInvertir > 0)
+{
+    numeroInvertido = numeroInvertido * 10 + (numeroAInvertir % 10);
+    numeroAInvertir /= 10;
+}
+Console.WriteLine("El número invertido es: " + numeroInvertido);
