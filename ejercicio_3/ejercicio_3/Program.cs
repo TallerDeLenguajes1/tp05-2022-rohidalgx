aa5e4a0c0a483d154d2645db5279c261907a8718
Rocio Hidalgo :P <85712858+rohidalgx@users.noreply.github.com>
2022-05-26 12:23:01 -0300

    Add files via upload
    calculadora2

---
+++ ejercicio_3/ejercicio_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+float primerNumero, segundoNumero;
+
+Console.WriteLine("Ingrese un número");
+primerNumero = (float)Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Valor Absoluto: " + Math.Abs(primerNumero));
+Console.WriteLine("Valor Cuadrado: " + Math.Pow(primerNumero, 2));
+Console.WriteLine("Raíz Cuadrada: " + Math.Pow(primerNumero, 0.5f));
+Console.WriteLine("Seno: " + Math.Sin(primerNumero));
+Console.WriteLine("Coseno: " + Math.Cos(primerNumero));
+Console.WriteLine("Parte Entera " + Math.Floor(primerNumero));
+
+Console.WriteLine("Ingrese otro número");
+primerNumero = (float)Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Ingrese el segundo número");
+segundoNumero = (float)Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("El máximo entre los dos es: " + Math.Max(primerNumero, segundoNumero));
+Console.WriteLine("El mínimo entre los dos es: " + Math.Min(primerNumero, segundoNumero));
