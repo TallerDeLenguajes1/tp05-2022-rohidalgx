2ca630f1a4912300fadf7e2ee693f7afd8dc09ae
Rocio Hidalgo Bini <hidalgobinirocio@gmail.com>
2022-06-06 12:39:19 -0300

    termine

---
+++ ejercicio_5/ejercicio_5/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+string? Cadena;
+
+Regex EmailRegex = new Regex(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+Regex WebRegex = new Regex(@"(?<Protocol>\w+):\/\/(?<Domain>[\w@][\w.:@]+)\/?[\w\.?=%&=\-@/$,]*");
+
+Console.WriteLine("Ingrese la cadena");
+Cadena = Console.ReadLine();
+
+Match EmailMatch = EmailRegex.Match(Cadena);
+Match WebMatch = WebRegex.Match(Cadena);
+
+if (EmailMatch.Success || WebMatch.Success)
+{
+    Console.WriteLine("Es correcta la cadena");
+}
+else
+{
+    Console.WriteLine("No es correcta");
+}
